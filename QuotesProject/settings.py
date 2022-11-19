260867836278bd4c40ad6460caa2f5385a90c8e5
deji12 <adesolaayodeji53@gmail.com>
2022-11-20 04:41:45 +0800

    deploying to railway

--- QuotesProject/settings.py
+++ QuotesProject/settings.py
@@ -76,8 +76,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
